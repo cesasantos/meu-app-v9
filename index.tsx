abe3f5c61124b14ba9bffff63d8454a9567e2161
cesasantos <161261283+cesasantos@users.noreply.github.com>
2025-10-19 12:27:44 -0300

    Import CSS file for styling

--- index.tsx
+++ index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { LanguageProvider } from './contexts/LanguageContext';
+import './index.css';
+
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
