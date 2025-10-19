b50839fa44631b7db2a8298dbc345ccddfd21438
cesasantos <161261283+cesasantos@users.noreply.github.com>
2025-10-19 12:39:29 -0300

    Remove index.css import from index.tsx

--- index.tsx
+++ index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { LanguageProvider } from './contexts/LanguageContext';
-import './index.css';
 
 
 const rootElement = document.getElementById('root');
